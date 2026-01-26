a7c5ec129c33325ead7cdf88cbe754576f20147a
makotosawada <makotosawada@me.com>
2026-01-26 16:49:41 +0900

    Create proceedings.ts

---
+++ src/data/proceedings.ts
@@ -0,0 +1,18 @@
+export const proceedings = [
+	{
+		title: '',
+		authors: '',
+		journal: '',
+		time: '',
+		link: '',
+		abstract: '',
+	},
+	// {
+	// 	title: 'The Radiation of Uranium Compounds',
+	// 	authors: 'Marie Curie',
+	// 	journal: 'Comptes Rendus de l’Académie des Sciences',
+	// 	time: '1898',
+	// 	link: '#',
+	// 	abstract: 'Early research leading to the identification of uranium’s radioactive properties.',
+	// },
+];
