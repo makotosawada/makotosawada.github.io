af750af25338a7c1f1312258c528cc7037e68aae
makotosawada <makotosawada@me.com>
2026-01-26 22:53:17 +0900

    Update settings.ts

--- src/settings.ts
+++ src/settings.ts
@@ -9,21 +9,21 @@ export const profile = {
 	  role: 'In-Flight Calibration Co-Lead, XRISM Team',
 	  summary: 'Securing mission success through rigorous detector science.',
       description: 'I ensure the success of the Resolve high-resolution spectrometer on the X-ray Imaging and Spectroscopy Mission (XRISM) by resolving systemic vulnerabilities through rigorous detector science. This expertise was demonstrated by identifying a conceptual misalignment in the design of the onboard calibration sources (Modulated X-ray Sources: MXS) for in-orbit gain tracking, and subsequently redefining the operational strategies to match the microcalorimeter event processing before the launch of XRISM. While the MXS is currently sidelined due to the gate-valve closed configuration, the alternative operational pattern I developed has enabled the full performance of Resolve through gain tracking using the filter wheel radioactive sources, securing the scientific foundation of the mission.',
-      field: 'instrumentation' 
+      field: 'det' 
     },
 	{ 
       title: 'Astrophysical Observations and Discovery', 
 	  role: 'Target Team Lead (W49B), XRISM Collaboration',
 	  summary: 'Transforming subtle data signatures into robust astrophysical discovery.',
       description: 'I leverage the unprecedented spectral resolution of the microcalorimeter spectrometer Resolve on XRISM to uncover hidden astrophysical features in hot, collisional, highly non-equilibrium plasma. In this era of precision science, breakthroughs often reside in subtle spectral signatures. I apply my expertise in detector response and systematic uncertainty in atomic codes to rigoroysly distinguish unexpected physical phenomena, transforming serendipitous finidings into validated discoveries. This approach is exemplified by my recent discovery of kinematic evidence for unique, bipolar ejecta flows in the supernova remnant W49B — a finding recognized as one of the major scientific highlights from the initial phase of the XRISM mission.', 
-      field: 'observation' 
+      field: 'obs' 
     },
     { 
       title: 'Laboratory Astrophysics for New Spectral Features', 
 	  role: 'Science PI, NASA ROSES APRA',
 	  summary: 'Establishing the physical groundwork to interpret novel spectral features.',
       description: 'I lead a laboratory astrophysics program to focus on identifying spectral features that originate from new astrophysical phenomena. Based on my original idea, I successfully principle-proven a novel method to experimentally simulate emission from non-equilibrium plasma such as non-Maxwellian electrons and overionized/recombining ions. Utilizing Electron Beam Ion Trap (EBIT) facilities through collaborations with LLNL and NASA/GSFC, this work provides the robust physical integrity necessary to distinguish new astrophysical discoveries from theoretical uncertainties. Supported by a NASA ROSES APRA grant, for which I serve as Science PI, this foundational research ensures that the unprecedented resolution of ongoing XRISM/Resolve and future NewAthena/X-IFU spectrometers is fully translated into validated scientific breakthroughs.', 
-      field: 'labastro' 
+      field: 'exp' 
     },
     ],
 }
