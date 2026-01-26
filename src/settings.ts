61eed1965044f35bfee18b7b588141a2c7f1c727
makotosawada <makotosawada@me.com>
2026-01-26 18:51:38 +0900

    Update settings.ts

--- src/settings.ts
+++ src/settings.ts
@@ -3,9 +3,31 @@ export const profile = {
 	title: 'Assistant Professor',
 	institute: 'Rikkyo University',
 	author_name: 'Makoto Sawada', // Author name to be highlighted in the papers section
-	research_areas: [
-		// { title: 'Physics', description: 'Brief description of the research interest', field: 'physics' },
-	],
+    research_areas: [
+    { 
+      title: 'Instrumentation & Calibration', 
+      description: 'Established regorous calibration methods for microcalorimeter high-resolutoin spectrometers. Identified critical design oversights of onboard calibration X-ray sources and resolved them by improving operational strategies prior to launch, ', 
+      field: 'instrumentation' 
+    },
+    { 
+      title: 'Systematic Risk Mitigation & Mission Recovery', 
+      field: 'mission-integrity',
+      description: 'Ensuring scientific success by identifying systemic vulnerabilities and developing multi-layered contingencies.\n\n' +
+        '• Design Correction & Backup Synthesis: Resolved critical flaws in Instrument A; developed Methodology D which ultimately secured Instrument C’s full performance after Component B’s failure.\n' +
+        '• Pre-launch Systematic Audit: Identified undetected architectural inconsistencies and implemented emergency software patches to safeguard observation integrity.\n' +
+        '• Post-launch Anomalous Resolution: Bridged the gap between instrumentation and observation to recover data from hardware anomalies.'
+    },
+	{ 
+      title: 'X-ray Spectroscopy of Non-Equilibrium Plasma', 
+      description: '.', 
+      field: 'observation' 
+    },
+    { 
+      title: 'Laboratory Astrophysics', 
+      description: 'Providing the rigorous physical basis that transforms flashy science headlines into validated, enduring breakthroughs.', 
+      field: 'laboratory' 
+    },
+    ],
 }
 
 // Set equal to an empty string to hide the icon that you don't want to display
